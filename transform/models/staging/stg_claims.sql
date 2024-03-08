fed141287555405b531ebba3a87bd601fa6db899
johalnes <johalnes@me.com>
2024-03-08 12:18:03 +0100

    Using Column names instead of *

--- transform/models/staging/stg_claims.sql
+++ transform/models/staging/stg_claims.sql
@@ -1,4 +1,18 @@
-with renamed as (select * from {{ source("input", "fact_claims") }})
+with
+    renamed as (
+        select
+            product_line_id,
+            business_unit_id,
+            make_date(
+                accidentyear, cast(1 + floor(12 * random()) as int), 20
+            ) as accident_date,
+            make_date(
+                developmentyear, cast(1 + floor(12 * random()) as int), 20
+            ) as development_date,
+            incurloss,
+            cumpaidloss
+        from {{ source("input", "fact_claims") }}
+    )
 
 select *
 from renamed
--- transform/models/staging/stg_premiums.sql
+++ transform/models/staging/stg_premiums.sql
@@ -1,4 +1,17 @@
-with renamed as (select * from {{ source("input", "fact_premiums") }})
+with
+    renamed as (
+        select
+            product_line_id,
+            business_unit_id,
+            make_date(
+                accidentyear, cast(1 + floor(12 * random()) as int), 20
+            ) as accident_date,
+            make_date(
+                developmentyear, cast(1 + floor(12 * random()) as int), 20
+            ) as development_date,
+            earnedpremdir as earned_premium_usd
+        from {{ source("input", "fact_premiums") }}
+    )
 
 select *
 from renamed
